685e3d890174ddcac64a8efc5f46685f2f8e9ad5
Mihailo Joksimovic <tinzey@gmail.com>
2015-01-22 20:42:04 +0100

    Initial Commit

---
+++ src/controllers.php
@@ -0,0 +1,50 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+$app->get('/', function () use ($app) {
+    $socks = array(
+        array(
+            'name'  => 'Crne Dugacke',
+            'image' => 'black_long_socks.jpg'
+        ),
+        array(
+            'name'  => 'Crne Kratke',
+            'image' => 'black_long_socks.jpg'
+        ),
+        array(
+            'name'  => 'Bele Kratke',
+            'image' => 'black_long_socks.jpg'
+        ),
+        array(
+            'name'  => 'Bele Dugacke',
+            'image' => 'black_long_socks.jpg'
+        ),
+    );
+
+    return $app['twig']->render('index.twig', array('socks' => $socks));
+})
+->bind('homepage')
+;
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.twig',
+        'errors/'.substr($code, 0, 2).'x.twig',
+        'errors/'.substr($code, 0, 1).'xx.twig',
+        'errors/default.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
